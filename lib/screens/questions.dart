f1daf1c384b25f720d97a92159d5f32bc80adbd1
sudipeb <sudip.koirala@ebpearls.com>
2025-10-08 11:20:00 +0545

    added the google fonts to the font family

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp/answer_button.dart';
 import 'package:quizapp/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,21 +11,45 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
-  final currentQuestions = questions[0];
+  var currentQuestionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      //increements the value of currentQuestionIndex by 1
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currentQuestions = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(currentQuestions.text, style: TextStyle(color: Colors.white)),
-          SizedBox(height: 20),
-          AnswerButton(onTap: () {}, answerText: currentQuestions.answers[0]),
-          AnswerButton(onTap: () {}, answerText: currentQuestions.answers[1]),
-          AnswerButton(onTap: () {}, answerText: currentQuestions.answers[2]),
-          AnswerButton(onTap: () {}, answerText: currentQuestions.answers[3]),
-        ],
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestions.text,
+              style: GoogleFonts.lato(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            //... is the spread operator
+            // it is used to spread the list of widgets
+            // into the children of the column
+            // without the spread operator, we would have to
+            // use separate answer buttons for each widget to the children
+            ...currentQuestions.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: answerQuestion);
+            }),
+          ],
+        ),
       ),
     );
   }
